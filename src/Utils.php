5aad1d0b8d7a3d93d957bb0ae29f0aac322c165e
Bosun18 <110723359+Bosun18@users.noreply.github.com>
2023-08-23 12:21:56 +0300

    Update Utils.php

--- src/Utils.php
+++ src/Utils.php
@@ -3,7 +3,7 @@
 namespace Hexlet\Phpunit\Utils;
 
 // Эта функция переворачивает переданную строку
-function reverseString($string)
+function reverseString($string): String
 {
     return implode(array_reverse(str_split($string)));
 }
